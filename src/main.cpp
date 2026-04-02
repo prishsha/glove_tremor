258b1e4e8d34c52fe75a89751b23a5c243c3a72b
prishsha <prishavadhavkar@gmail.com>
2026-04-02 23:44:03 +0530

    five led

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,19 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
 
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-
-// 🔴 Replace with YOUR IP
-const char* server = "http://IP:5000/esp";
-
-#define MOTOR_PIN 18
+int pins[5] = {18, 19, 21, 22, 23};
 
 void setup() {
-  Serial.begin(115200);
-
-  pinMode(MOTOR_PIN, OUTPUT);
-
-  WiFi.begin(ssid, password);
-
-  Serial.print("Connecting to WiFi");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  for (int i = 0; i < 5; i++) {
+    pinMode(pins[i], OUTPUT);
   }
-
-  Serial.println("\nConnected!");
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-
-    HTTPClient http;
-    http.begin(server);
-
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode > 0) {
-      String payload = http.getString();
-
-      Serial.println(payload);
-
-      // simple check
-      if (payload.indexOf("\"motor\":1") > 0) {
-        digitalWrite(MOTOR_PIN, HIGH);
-      } else {
-        digitalWrite(MOTOR_PIN, LOW);
-      }
-    }
-
-    http.end();
+  // RANDOM PATTERN (like glove motors)
+  for (int i = 0; i < 5; i++) {
+    int state = random(0, 2);  // 0 or 1
+    digitalWrite(pins[i], state);
   }
 
-  delay(500);
+  delay(300);
 }
