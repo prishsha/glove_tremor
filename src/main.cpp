6a942909639ed792b6520f9f2610deb2cedb5926
prishsha <prishavadhavkar@gmail.com>
2026-04-02 23:36:14 +0530

    integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// 🔴 Replace with YOUR IP
+const char* server = "http://IP:5000/esp";
 
 #define MOTOR_PIN 18
 
-float freq = 6.0;
-float t = 0;
-
-float threshold = 0.6;   // tremor detection threshold
-
 void setup() {
   Serial.begin(115200);
+
   pinMode(MOTOR_PIN, OUTPUT);
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
 }
 
 void loop() {
-  Serial.println("TEST OUTPUT");
-  delay(1000);
+  if (WiFi.status() == WL_CONNECTED) {
+
+    HTTPClient http;
+    http.begin(server);
+
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      String payload = http.getString();
+
+      Serial.println(payload);
+
+      // simple check
+      if (payload.indexOf("\"motor\":1") > 0) {
+        digitalWrite(MOTOR_PIN, HIGH);
+      } else {
+        digitalWrite(MOTOR_PIN, LOW);
+      }
+    }
+
+    http.end();
+  }
+
+  delay(500);
 }
